f2691af42bfa6ed0867f7b6f3b67c6c9b86df844
AugustinJose1221 <44978968+AugustinJose1221@users.noreply.github.com>
2019-12-14 10:51:32 +0530

    Update Context_Word_Predict.py

--- Context_Word_Predict.py
+++ Context_Word_Predict.py
@@ -1,6 +1,7 @@
 from gensim.models import Word2Vec
 import multiprocessing
 loaded_model = Word2Vec.load('model.bin')
-context_words_list = ['He','went','to','get','a']
-target_words = loaded_model.predict_output_word(context_words_list, topn=10)  #Predicting context words for the list of target words
+def Target_Word(context_words_list):
+    target_words = loaded_model.predict_output_word(context_words_list, topn=10)  #Predicting context words for the list of target words
+    return target_words
 
