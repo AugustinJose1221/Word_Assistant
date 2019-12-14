c1187206dc65d26396864f81533724a78ba8c3a9
AugustinJose1221 <44978968+AugustinJose1221@users.noreply.github.com>
2019-12-14 11:00:53 +0530

    Update Context_Word_Predict.py

--- Context_Word_Predict.py
+++ Context_Word_Predict.py
@@ -1,7 +1,16 @@
 from gensim.models import Word2Vec
 import multiprocessing
+import string
 loaded_model = Word2Vec.load('model.bin')
 def Target_Word(context_words_list):
     target_words = loaded_model.predict_output_word(context_words_list, topn=10)  #Predicting context words for the list of target words
     return target_words
+def Word_List(sentence):
+    words = sentence.split()
+    string_map = str.maketrans('', '', string.punctuation)
+    word_list = [w.translate(string_map) for w in words]
+    return word_list
+
+
+
 
